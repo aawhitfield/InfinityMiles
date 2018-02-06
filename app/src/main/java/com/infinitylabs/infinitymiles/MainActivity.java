eff088f7285de368f8899d86a2ff2e481060d4d7
Aaron Whitfield <aawhitfield@hotmail.com>
2018-02-05 22:50:27 -0800

    main menu

--- app/src/main/java/com/infinitylabs/infinitymiles/MainActivity.java
+++ app/src/main/java/com/infinitylabs/infinitymiles/MainActivity.java
@@ -1,9 +1,17 @@
 package com.infinitylabs.infinitymiles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void playBattleship(View view)
+    {
+        Intent battleshipIntent = new Intent(this, BattleshipActivity.class);
+        startActivity(battleshipIntent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
