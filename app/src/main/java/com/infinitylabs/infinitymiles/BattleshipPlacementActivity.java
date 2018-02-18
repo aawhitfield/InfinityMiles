81c731b35f8bd42d52f6f0fa68320ba1477d1e8e
Aaron Whitfield <aawhitfield@hotmail.com>
2018-02-17 23:14:36 -0800

    started being able to layout ship placement

--- app/src/main/java/com/infinitylabs/infinitymiles/BattleshipPlacementActivity.java
+++ app/src/main/java/com/infinitylabs/infinitymiles/BattleshipPlacementActivity.java
@@ -3,15 +3,22 @@ package com.infinitylabs.infinitymiles;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BattleshipPlacementActivity extends AppCompatActivity {
+
+    public void guessOpponents(View view)
+    {
+        Intent battleshipIntent = new Intent(this, BattleshipActivity.class);
+        startActivity(battleshipIntent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battleship_placement);
 
-        Intent battleshipIntent = new Intent(this, BattleshipActivity.class);
-        startActivity(battleshipIntent);
+
     }
 }
