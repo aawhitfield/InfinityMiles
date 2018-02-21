2e64a9156f178d1024873a3e187631f25a387396
Aaron Whitfield <aawhitfield@hotmail.com>
2018-02-20 21:57:12 -0800

    added all of the states

--- app/src/main/java/com/infinitylabs/infinitymiles/LicensePlateActivity.java
+++ app/src/main/java/com/infinitylabs/infinitymiles/LicensePlateActivity.java
@@ -31,6 +31,39 @@ public class LicensePlateActivity extends AppCompatActivity {
         stateList.add("Kansas");
         stateList.add("Kentucky");
         stateList.add("Louisiana");
+        stateList.add("Maine");
+        stateList.add("Maryland");
+        stateList.add("Massachusetts");
+        stateList.add("Michigan");
+        stateList.add("Minnesota");
+        stateList.add("Mississippi");
+        stateList.add("Missouri");
+        stateList.add("Montana");
+        stateList.add("Nebraska");
+        stateList.add("Nevada");
+        stateList.add("New Hampshire");
+        stateList.add("New Jersey");
+        stateList.add("New Mexico");
+        stateList.add("New York");
+        stateList.add("North Carolina");
+        stateList.add("North Dakota");
+        stateList.add("Ohio");
+        stateList.add("Oklahoma");
+        stateList.add("Oregon");
+        stateList.add("Pennsylvania");
+        stateList.add("Rhode Island");
+        stateList.add("South Carolina");
+        stateList.add("South Dakota");
+        stateList.add("Tennessee");
+        stateList.add("Texas");
+        stateList.add("Utah");
+        stateList.add("Vermont");
+        stateList.add("Virginia");
+        stateList.add("Washington");
+        stateList.add("West Virginia");
+        stateList.add("Wisconsin");
+        stateList.add("Wyoming");
+
     }
 
 
