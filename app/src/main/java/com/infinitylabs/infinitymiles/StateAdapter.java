9b9223bd410eef3660d7ad7f21b1904c7cdc0c1a
Aaron Whitfield <aawhitfield@hotmail.com>
2018-03-15 22:41:05 -0700

    Added permanent storage for isFound for States, but DO NOT YET read from storage. That's what needs to be implemented next.

--- app/src/main/java/com/infinitylabs/infinitymiles/StateAdapter.java
+++ app/src/main/java/com/infinitylabs/infinitymiles/StateAdapter.java
@@ -1,6 +1,7 @@
 package com.infinitylabs.infinitymiles;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,23 +12,19 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import static com.infinitylabs.infinitymiles.R.drawable.californiia;
 
 /**
- * Created by Ted on 3/6/2018.
+ * Created by Aaron Whitfield on 3/6/2018.
  */
 
 public class StateAdapter extends ArrayAdapter<State> {
     public StateAdapter(@NonNull Context context, State[] states) {
         super(context, R.layout.state_row, states);
-    }
-
-    public void toggleCheckmarks()
-    {
-
     }
 
     @NonNull
@@ -55,15 +52,34 @@ public class StateAdapter extends ArrayAdapter<State> {
         stateCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(compoundButton.isChecked())
-                {
+                if(compoundButton.isChecked()) {
                     b = true;
                     getItem(position).isFound = true;
+
+                    SharedPreferences sharedPref = getContext().getApplicationContext().getSharedPreferences("com.infinitylabs.infinitymiles", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putBoolean(getItem(position).name, getItem(position).isFound);
+                    editor.apply();
+
+                    // Boolean myStateStatus = sharedPref.getBoolean(getItem(position).name, false);
+
+
+                    //Toast.makeText(getContext().getApplicationContext(), "" + getItem(position).name + " is " + myStateStatus, Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
                     b = false;
                     getItem(position).isFound = false;
+
+                    SharedPreferences sharedPref = getContext().getApplicationContext().getSharedPreferences("com.infinitylabs.infinitymiles", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putBoolean(getItem(position).name, getItem(position).isFound);
+                    editor.apply();
+
+                    // Boolean myStateStatus = sharedPref.getBoolean(getItem(position).name, false);
+
+
+                    //Toast.makeText(getContext().getApplicationContext(), "" + getItem(position).name + " is " + myStateStatus, Toast.LENGTH_SHORT).show();
                 }
             }
         });
